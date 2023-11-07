d7165af91e60d5aa128df2c5dbcb61c4bb93eacf
Era Mae Fulgencio <148873991+eramaef@users.noreply.github.com>
2023-11-07 12:18:02 +0000

    task.1.1.3

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
+public class TaskSheet113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message;
+
+        for (int i = 0; i <= check_messages; i++) {
+            message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
+            System.out.println(message);
+        }
+    }
+}
 
